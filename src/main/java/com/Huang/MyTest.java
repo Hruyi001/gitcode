60b516727b6e83b717a5f9b0c9cf63cdbbb0a589
Hruyi <2917398357@qq.com>
2022-03-30 11:16:49 +0800

    first Commit

---
+++ src/main/java/com/Huang/MyTest.java
@@ -0,0 +1,11 @@
+package com.Huang;
+
+/**
+ * @author HuangRuYi
+ * @date 2022/3/30 - 11:13
+ */
+public class MyTest {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
