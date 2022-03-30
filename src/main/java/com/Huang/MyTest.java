52269f54f74e44e2a86e1025def94d257d7008dc
Hruyi <2917398357@qq.com>
2022-03-30 11:32:04 +0800

    hot-fix Commit1

--- src/main/java/com/Huang/MyTest.java
+++ src/main/java/com/Huang/MyTest.java
@@ -7,5 +7,9 @@ package com.Huang;
 public class MyTest {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
     }
 }
