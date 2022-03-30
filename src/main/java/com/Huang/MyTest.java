6c5c1e1c37a2ce18cac318cffc0117acfb523fdf
Hruyi <2917398357@qq.com>
2022-03-30 11:21:02 +0800

    second Commit

--- src/main/java/com/Huang/MyTest.java
+++ src/main/java/com/Huang/MyTest.java
@@ -7,5 +7,6 @@ package com.Huang;
 public class MyTest {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello World2");
     }
 }
